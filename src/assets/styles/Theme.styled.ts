1051a5ff57f8d4ca1a152e7e9353e6129dce571e
ibraheemsulay <ibraheemsulay@gmail.com>
2022-06-02 10:51:43 +0100

    added the colors to the theme

--- src/assets/styles/Theme.styled.ts
+++ src/assets/styles/Theme.styled.ts
@@ -1,8 +1,11 @@
 const theme = {
-  bgCol: "yellow",
-  pryCol: "blue",
-  secCol: "green",
-  navBgCol: "brown",
+  bgCol: 'rgb(244, 245, 249)',
+  pryCol: 'rgba(66, 71, 74, 1)', // text color
+  secCol: 'rgba(162, 164, 185, 1)', //nav color
+  terCol: 'rgba(136, 140, 183, 1)', //hello color
+  btnCol: 'rgba(113, 59, 219, 1)', //purple
+  greenCol: ' linear-gradient(90deg, #33D69F 0%, #50EDB8 111.47%)', //greeen
+  navBgCol: 'brown',
 };
 
 export type ThemeType = typeof theme;
